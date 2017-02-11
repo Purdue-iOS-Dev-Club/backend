d04fb1cb9cbfcaf1d71c10afa850958d84b78318
whuang8 <huang554@purdue.edu>
2017-02-10 19:34:40 -0500

    Add PostgreSQL package

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,9 +1,20 @@
 import Vapor
+import VaporPostgreSQL
+import HTTP
 
 let drop = Droplet()
+try drop.addProvider(VaporPostgreSQL.Provider.self)
 
 drop.get { req in
     return JSON(["Status": "Success"])
+}
+
+drop.get("db_version") { request in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT Version()")
+        return JSON(version)
+    }
+    return "Error connecting to db"
 }
 
 drop.run()
